c6cbb94c86df0dbd6e4014536edcfbeaa615ed5b
Sogutt <sogutt@bu.edu>
2018-03-25 20:52:08 -0400

    one more update

--- config.js
+++ config.js
@@ -10,6 +10,6 @@
 module.exports = {
   consumer_key: 'TJivXZQo',  
   consumer_secret: 'PoKsxvZquXCDEGqLMT7OntE6T',
-  access_token: '3412426126-xHm8s2y8KjVtV3FTP9ngGQFc3cBKdTjh4vhU73H',  
-  access_token_secret: 'Oa0n1bGsymxZpcyQudX5z7VwDyiLgbSv9IHZxhyGPhz33'
+  access_token: '3412426126-xTP9KdTjh4vhU73H',  
+  access_token_secret: 'Oa0n1bGsymxZIHZxhyGPhz33'
 }
